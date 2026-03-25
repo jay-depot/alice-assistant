643291ab5cfdfd8d27114c95b42730b60a4e073d
J <unleet.42@gmail.com>
2026-03-25 16:37:14 -0400

    Expose the plugin API when alice-assistant is imported

---
+++ src/lib.ts
@@ -0,0 +1,5 @@
+export * from './lib/types/alice-plugin-interface.js';
+export * from './lib/types/system-config-full.js';
+export * from './lib/dynamic-prompt.js';
+export * from './lib/conversation.js';
+export * from './lib/tool-system.js';
